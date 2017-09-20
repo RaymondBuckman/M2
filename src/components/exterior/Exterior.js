4caeda9332ac2d702540622776e828f1cf08cb0b
Raymond <dude92128@aol.com>
2017-09-19 17:00:24 -0700

    Animated h1/p

--- src/components/exterior/Exterior.js
+++ src/components/exterior/Exterior.js
@@ -8,8 +8,8 @@ class Exterior extends Component {
             <div className="Seven" id="Exterior">
                 <div className="container">   
                     <div className="row">
-                        <h1>Exterior</h1>
-                        <p className="col-xs-10 col-xs-offset-1 col-md-4 col-md-offset-0">“It goes without staying (see what I did there?) that we love how the compact two-door looks ready to pounce, with its muscular, blistered fenders, and ready to suck the lane markers off the road with all those air intakes, cooling ports, and grille."<br/><br/>&#160;&#160;&#160;- MotorTrend</p>
+                        <h1 className="scrollmagic-enter-from-left">Exterior</h1>
+                        <p className="col-xs-10 col-xs-offset-1 col-md-4 col-md-offset-0 scrollmagic-enter-from-left">“It goes without staying (see what I did there?) that we love how the compact two-door looks ready to pounce, with its muscular, blistered fenders, and ready to suck the lane markers off the road with all those air intakes, cooling ports, and grille."<br/><br/>&#160;&#160;&#160;- MotorTrend</p>
                     </div>
                 </div>
             </div>

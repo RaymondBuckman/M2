879f1c382eaa27f799a7dad30c7a8cde4dd4d855
Raymond <dude92128@aol.com>
2017-09-19 17:01:28 -0700

    Animated h1/p

--- src/components/performance/Performance.js
+++ src/components/performance/Performance.js
@@ -8,8 +8,8 @@ class Performance extends Component {
             <div className="Six" id="Performance">
                 <div className="container">
                     <div className="row">
-                        <h1>Performance</h1>
-                        <p className="col-xs-10 col-xs-offset-2 text-center col-md-4 col-md-offset-8 text-right col-lg-4 col-lg-offset-8 text-right">"With 0.99 g worth of grip available, quick and responsive steering, and front buckets that comfortably restrain the occupants, the BMW M2 is a 200-proof driving elixir."<br/><br/>&#160;&#160;&#160;- Car And Driver</p>
+                        <h1 className="scrollmagic-enter-from-right">Performance</h1>
+                        <p className="col-xs-10 col-xs-offset-2 text-center col-md-4 col-md-offset-8 text-right col-lg-4 col-lg-offset-8 text-right scrollmagic-enter-from-right"> "With 0.99 g worth of grip available, quick and responsive steering, and front buckets that comfortably restrain the occupants, the BMW M2 is a 200-proof driving elixir."<br/><br/>&#160;&#160;&#160;- Car And Driver</p>
                     </div>
                 </div>
             </div>
